2bdff3aff78a6eac276c1c090d1e27157c3c9cd2
Sherimov <vasil.sherimov@yahoo.com>
2020-03-04 16:40:20 +0100

    Trafic lights

---
+++ Homework-01/Homework-01/Traffic light/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Traffic_light
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trafic lights...!");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red light - STOP !!!");
+            Thread.Sleep(6000);
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow - Prepare !!!");
+            Thread.Sleep(3000);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green light - GO !!!");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
